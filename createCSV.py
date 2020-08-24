e161fe14c7f0058f8bee6c2a465d2bd3375948f2
Sean Smith <seaniksmith@gmail.com>
2020-08-24 13:54:32 +1000

    Updated csv script

--- createCSV.py
+++ createCSV.py
@@ -1,15 +1,33 @@
 import csv
 import sys
 
-with open('studentMarks.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+
+def inputData(filename):
+    with open(filename, 'a') as file:
+        rows = input("Row Variables (comma separated): ")
+        rows = rows.split(',')
+        rows = [i.strip() for i in rows]
+
+        writer = csv.DictWriter(file, fieldnames=rows)
+
+        while True:
+            try:
+                newRow = {}
+                for row in rows:
+                    variable = input(f"{row}: ")
+                    newRow[row] = variable
+                writer.writeheader()
+                writer.writerow(newRow)
+            except EOFError:
+                break
+
+
 
 def main():
     if len(sys.argv) == 1:
-        print("Usage: python3 cr")
-    pass
+        print("Usage: python3 createCSV.py <fileName>")
+    else:
+        inputData(sys.argv[1])
 
 if __name__ == "__main__":
     main()
